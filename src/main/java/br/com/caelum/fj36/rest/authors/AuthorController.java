fdbff967c4988003dd5126b87581085815f8635d
Fernando <feh.wilinando@gmail.com>
2019-02-11 09:41:36 -0200

    Make AuthorController as Annotation

--- src/main/java/br/com/caelum/fj36/rest/authors/AuthorController.java
+++ src/main/java/br/com/caelum/fj36/rest/authors/AuthorController.java
@@ -1,65 +1,15 @@
 package br.com.caelum.fj36.rest.authors;
 
-import br.com.caelum.fj36.rest.shared.models.Author;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.Optional;
-
-import static org.springframework.http.ResponseEntity.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @RestController
 @RequestMapping("authors")
-public class AuthorController {
-
-    private final Converter<AuthorDTO, Author> converter;
-    private final AuthorRepository repository;
-
-    public AuthorController(Converter<AuthorDTO, Author> converter, AuthorRepository repository) {
-        this.converter = converter;
-        this.repository = repository;
-    }
-
-    @PostMapping
-    public ResponseEntity<?> create(@RequestBody @Valid AuthorDTO authorDTO) {
-        Author author = converter.convert(authorDTO);
-
-        repository.save(author);
-
-        URI uri = UriComponentsBuilder.newInstance().path("/authors/{id}").buildAndExpand(author.getId()).toUri();
-
-        return created(uri).body(author);
-    }
-
-
-    @GetMapping(value = "{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public ResponseEntity<Author> findById(@PathVariable Long id) {
-        return repository.findById(id)
-                .map(ok()::body)
-                .orElseGet(notFound()::build);
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteAuthorById(@PathVariable Long id) {
-        Optional<Author> optionalAuthor = repository.findById(id);
-
-        optionalAuthor.ifPresent(repository::delete);
-
-        if (optionalAuthor.isPresent()) {
-            return noContent().build();
-        }
-
-        return notFound().build();
-    }
-}
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AuthorController { }
