26130f7e29b599717902ab324eb984d0e353ed1a
Fernando <feh.wilinando@gmail.com>
2019-02-20 19:08:22 -0300

    Fix annotation and produces resttemplate

--- src/main/java/br/com/caelum/fj36/rest/books/BookExceptionHandler.java
+++ src/main/java/br/com/caelum/fj36/rest/books/BookExceptionHandler.java
@@ -2,14 +2,13 @@ package br.com.caelum.fj36.rest.books;
 
 import br.com.caelum.fj36.rest.configurations.ErrorResponse;
 import br.com.caelum.fj36.rest.shared.exceptions.BookNotFoundException;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
-
-@ControllerAdvice(basePackageClasses = BookExceptionHandler.class)
+@RestControllerAdvice(basePackageClasses = BookExceptionHandler.class)
 public class BookExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
--- src/main/java/br/com/caelum/fj36/rest/configurations/WebConfiguration.java
+++ src/main/java/br/com/caelum/fj36/rest/configurations/WebConfiguration.java
@@ -1,15 +1,22 @@
 package br.com.caelum.fj36.rest.configurations;
 
+import javax.servlet.Filter;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-
-import javax.servlet.Filter;
 
 @Configuration
 public class WebConfiguration {
+
     @Bean
     public Filter etagFilter() {
         return new ShallowEtagHeaderFilter();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
